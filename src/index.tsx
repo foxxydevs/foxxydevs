c92d9254845afaf255e910a9f0f1e0a1c4715f66
ioofy <mynev.id@gmail.com>
2021-11-27 21:37:44 +0700

    update: configuration a index for typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './components/Global/__Global.scss';
+import App from './components/Layout/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('__main')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
