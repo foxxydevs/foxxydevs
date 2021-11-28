670ba1d33b4c6374b08c0a1003d7f057bf42b30a
ioofy <mynev.id@gmail.com>
2021-11-28 12:54:44 +0700

    update: adding header, and footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './__Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer-wrapper'>
+      <section className='footer'></section>
+    </div>
+  );
+};
+
+export default Footer;
